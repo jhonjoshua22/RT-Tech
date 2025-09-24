c0813c9b760193eb80897ab06fb25051015d0278
jhonjoshua.22 <sjosh2503@gmail.com>
2025-09-24 23:59:25 +0800

    updates

--- productlist.js
+++ productlist.js
@@ -4,35 +4,35 @@ export const topsellers = [
     rating: 4.8,
     image: "productimage/dell7450.png",
     title: "Dell Latitude 7450 ULTRA7 14 in",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
   { 
     brand: "Inspiron",
     rating: 4.9,
     image: "productimage/inspiron3530.png",
     title: "Inspiron READY FOR BUSINESS 3530 15 in",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
   {
     brand: "Dell",
     rating: 4.9,
     image: "productimage/delle2425hsm.png",
     title: "Dell Pro 24 Monitor - E2425HSM",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
   {
     brand: "Dell",
     rating: 4.8,
     image: "productimage/dell5455.png",
     title: "Dell Latitude 5455 Snapdragon(R) X Plus",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
   {
     brand: "Dell",
     rating: 4.8,
     image: "productimage/dell13gen.png",
     title: "Dell Latitude 3450 13th Generation 14in",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
 ];
 
@@ -42,35 +42,35 @@ export const newarrival = [
     rating: "--",
     image: "productimage/dell7020.jpg",
     title: "Dell OptiPlex Small Form Factor 7020",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
   { 
     brand: "Dell",
     rating: 4.8,
     image: "productimage/dell13gen.png",
     title: "Dell Latitude 3450 13th Generation 14in",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
   {
     brand: "Dell",
     rating: "--",
     image: "productimage/delle2020h.png",
     title: "Dell E2020H 19.45 in Widescreen Flat Panel Monitor",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
   {
     brand: "Dell",
     rating: 4.8,
     image: "productimage/dell7450.png",
     title: "Dell Latitude 7450 ULTRA7 14 in",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
   {
     brand: "Dell",
     rating: "--",
     image: "productimage/dell7455.png",
     title: "Dell Latitude 7455 Snapdragon(R) X Plus",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
 ];
 
@@ -80,34 +80,34 @@ export const topPicks = [
     rating: 4.9,
     image: "productimage/delle2425hsm.png",
     title: "Dell Pro 24 Monitor - E2425HSM",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
   { 
     brand: "Dell",
     rating: 4.8,
     image: "productimage/dell5455.png",
     title: "Dell Latitude 5455 Snapdragon(R) X Plus",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
   {
     brand: "Dell",
     rating: "--",
     image: "productimage/dell7020.jpg",
     title: "Dell OptiPlex Small Form Factor 7020",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
   {
     brand: "Inspiron",
     rating: 4.9,
     image: "productimage/inspiron3530.png",
     title: "Inspiron READY FOR BUSINESS 3530 15 in",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
   {
     brand: "Dell",
     rating: "--",
     image: "productimage/delle2020h.png",
     title: "Dell E2020H 19.45 in Widescreen Flat Panel Monitor",
-    buttonText: "Product Description"
+    buttonText: "Description"
   },
 ];
